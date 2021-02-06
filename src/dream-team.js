fd24bf791b931ae4e24eff3240463d941b8b5a0e
evilcore29 <evilcore29@gmail.com>
2021-02-06 18:43:51 +0300

    feat: implement createDreamTeam function

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,16 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!members || !Array.isArray(members)) return false;
+
+  const caseInsensitiveComp = (a, b) =>
+    a.toLowerCase().localeCompare(b.toLowerCase());
+
+  return members
+    .reduce((acc, item) => {
+      if (typeof item !== "string") return acc;
+      return [...acc, item.trim()[0].toLocaleUpperCase()];
+    }, [])
+    .sort((a, b) => caseInsensitiveComp(a, b))
+    .join("");
 };
