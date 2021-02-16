ce62cd7fc68e6640cbe5728860e8f15d1dba28b2
evilcore29 <evilcore29@gmail.com>
2021-02-16 18:32:01 +0300

    feat: implement extended-repeater and simple-chain functions

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,32 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
-  
+module.exports = function repeater(str, options) {
+  const convertableValues = {
+    null: "null",
+    undefined: "undefined",
+    false: "false"
+  };
+
+  const createStringWithAddition = () => {
+    const additionArr = [];
+    let counter = 0;
+    while (counter < (options.additionRepeatTimes || 1)) {
+      additionArr.push(
+        Object.values(convertableValues).includes(options.addition) || options.addition === null
+          ? convertableValues[options.addition]
+          : options.addition
+      );
+      counter++;
+    }
+    return `${str}${additionArr.join(options.additionSeparator || "")}`;
+  };
+
+  const resultArr = [];
+  let counter = 0;
+  while (counter < (options.repeatTimes || 1)) {
+    resultArr.push(createStringWithAddition());
+    counter++;
+  }
+
+  return resultArr.join(options.separator || "+");
+};  
